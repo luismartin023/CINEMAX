f32794bb2a7b939329f23645eab97efc9a83157b
Luis Martin Peña Mejia <luismartinpm2580@gmail.com>
2025-12-04 18:44:13 -0400

    Delete ConsoleApp3 directory

--- ConsoleApp3/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-awd
-    wda, flmekfmnklewmnfkemf;LockRecursionException, f;lock,fplkmel, f;enum,f;lkwp, w;MultipartFormDataContent
